202e1a2534c8f428f533fb5f06b5a9ae48f4667c
Oscar Streif <os.streif@gmail.com>
2026-05-05 23:41:48 +0200

    Add contour grid cross background

--- components/ShaderGradientBackground.tsx
+++ components/ShaderGradientBackground.tsx
@@ -1,80 +1,62 @@
 const CONTOUR_LOOPS = [
-  "M-152 4C-146-78-82-132 9-139C103-146 174-91 181-10C189 79 123 142 23 149C-78 156-159 96-152 4Z",
-  "M-124 2C-119-59-72-100 2-106C76-112 133-71 140-8C148 61 96 108 18 114C-61 120-130 73-124 2Z",
-  "M-96 1C-91-44-58-76-3-81C52-86 98-55 104-7C111 45 72 82 12 87C-47 92-101 55-96 1Z",
-  "M-69-2C-64-33-40-54 0-57C40-60 73-38 78-5C84 31 54 58 10 62C-33 66-74 38-69-2Z",
-  "M-43-4C-39-23-24-36 3-38C30-39 52-25 55-5C59 18 38 35 10 38C-17 40-47 22-43-4Z",
-  "M-21-4C-18-14-10-21 5-22C20-22 32-14 34-4C36 9 23 18 8 19C-7 20-24 10-21-4Z",
+  "M-190-17C-177-91-114-132-45-119C18-107 23-45 82-40C147-34 194 19 175 83C154 154 59 151 2 119C-54 88-76 31-133 44C-170 52-198 31-190-17Z",
+  "M-158-12C-147-69-101-101-45-94C8-88 20-38 69-34C121-30 158 13 143 61C126 113 52 113 4 91C-44 68-62 23-108 34C-136 41-164 24-158-12Z",
+  "M-125-8C-115-50-79-74-36-69C6-64 18-31 58-27C95-23 121 10 110 45C96 84 38 84 2 70C-35 55-49 19-84 26C-107 31-130 19-125-8Z",
+  "M-92-6C-83-34-58-51-27-48C4-45 18-22 45-19C70-16 89 7 80 31C70 58 29 60 2 49C-25 38-37 14-61 18C-78 21-97 13-92-6Z",
+  "M-61-4C-55-23-38-34-17-32C4-30 16-15 34-13C52-11 64 5 58 20C50 38 22 39 2 33C-17 26-25 9-42 12C-52 14-64 8-61-4Z",
 ];
 
-const SMALL_CONTOUR_LOOPS = [
-  "M-88 0C-83-46-44-74 9-76C63-78 99-44 98 3C97 50 56 79 4 78C-49 77-93 48-88 0Z",
-  "M-66 1C-62-32-34-52 5-54C44-55 71-31 71 2C70 35 41 56 3 56C-36 55-70 35-66 1Z",
-  "M-43 0C-40-20-23-33 4-34C31-35 49-20 49 1C49 23 29 37 3 37C-23 36-46 22-43 0Z",
-  "M-22-1C-20-11-10-19 5-19C20-19 30-10 30 1C30 14 17 21 3 21C-12 20-24 12-22-1Z",
+const CHANNEL_LOOPS = [
+  "M-218-18C-188-96-99-117-29-69C42-20 103-67 169-37C232-9 219 66 156 93C93 121 44 86-15 110C-84 139-193 93-218-18Z",
+  "M-174-15C-149-72-83-87-27-51C31-14 83-47 135-25C180-5 171 48 124 69C76 90 38 63-12 82C-66 103-152 67-174-15Z",
+  "M-130-12C-112-50-61-61-20-35C23-8 62-29 99-14C130-1 124 35 89 50C55 65 28 47-9 60C-47 74-115 47-130-12Z",
+  "M-88-8C-76-32-42-39-14-22C14-5 42-17 65-8C85 0 80 22 58 31C36 40 19 30-7 38C-31 46-78 30-88-8Z",
 ];
 
 const CONTOUR_ISLANDS = [
-  { transform: "translate(116 74) rotate(-17) scale(0.72 0.52)", size: "small" },
-  { transform: "translate(318 92) rotate(11) scale(0.58 0.9)" },
-  { transform: "translate(535 65) rotate(-28) scale(0.52 0.42)", size: "small" },
-  { transform: "translate(735 112) rotate(19) scale(0.48 0.74)", size: "small" },
-  { transform: "translate(1010 92) rotate(-9) scale(0.72 0.48)" },
-  { transform: "translate(1305 112) rotate(14) scale(0.56 0.84)" },
-  { transform: "translate(1516 84) rotate(-20) scale(0.42 0.54)", size: "small" },
-  { transform: "translate(95 286) rotate(23) scale(0.54 0.86)" },
-  { transform: "translate(278 318) rotate(-11) scale(0.62 0.44)", size: "small" },
-  { transform: "translate(515 285) rotate(8) scale(0.84 0.62)" },
-  { transform: "translate(782 310) rotate(-5) scale(0.56 1.08)" },
-  { transform: "translate(1082 322) rotate(21) scale(0.76 0.56)" },
-  { transform: "translate(1358 292) rotate(-15) scale(0.52 0.66)", size: "small" },
-  { transform: "translate(1554 334) rotate(12) scale(0.44 0.82)" },
-  { transform: "translate(190 528) rotate(-27) scale(0.72 0.5)" },
-  { transform: "translate(432 558) rotate(15) scale(0.64 0.78)" },
-  { transform: "translate(674 514) rotate(-8) scale(0.48 0.56)", size: "small" },
-  { transform: "translate(918 548) rotate(18) scale(0.84 0.64)" },
-  { transform: "translate(1198 535) rotate(-19) scale(0.62 0.9)" },
-  { transform: "translate(1448 552) rotate(8) scale(0.52 0.58)", size: "small" },
-  { transform: "translate(70 760) rotate(8) scale(0.48 0.72)", size: "small" },
-  { transform: "translate(335 780) rotate(-14) scale(0.76 0.54)" },
-  { transform: "translate(602 760) rotate(22) scale(0.52 0.78)" },
-  { transform: "translate(860 786) rotate(-10) scale(0.42 0.52)", size: "small" },
-  { transform: "translate(1095 765) rotate(12) scale(0.7 0.5)" },
-  { transform: "translate(1398 782) rotate(-21) scale(0.66 0.84)" },
-  { transform: "translate(1605 742) rotate(13) scale(0.44 0.62)", size: "small" },
+  { transform: "translate(88 76) rotate(-11) scale(0.74 0.36)" },
+  { transform: "translate(328 88) rotate(8) scale(0.54 0.72)" },
+  { transform: "translate(555 72) rotate(-24) scale(0.58 0.34)", variant: "channel" },
+  { transform: "translate(782 112) rotate(16) scale(0.48 0.62)" },
+  { transform: "translate(1015 82) rotate(-7) scale(0.76 0.34)", variant: "channel" },
+  { transform: "translate(1312 122) rotate(13) scale(0.55 0.7)" },
+  { transform: "translate(1538 78) rotate(-18) scale(0.42 0.42)" },
+  { transform: "translate(98 286) rotate(19) scale(0.5 0.88)", variant: "channel" },
+  { transform: "translate(285 326) rotate(-13) scale(0.64 0.34)" },
+  { transform: "translate(510 288) rotate(6) scale(0.82 0.48)" },
+  { transform: "translate(778 312) rotate(-4) scale(0.44 1.02)" },
+  { transform: "translate(1085 325) rotate(20) scale(0.74 0.4)", variant: "channel" },
+  { transform: "translate(1358 292) rotate(-16) scale(0.48 0.62)" },
+  { transform: "translate(1554 338) rotate(10) scale(0.42 0.78)" },
+  { transform: "translate(184 536) rotate(-26) scale(0.78 0.34)", variant: "channel" },
+  { transform: "translate(430 556) rotate(14) scale(0.58 0.72)" },
+  { transform: "translate(676 512) rotate(-9) scale(0.44 0.5)" },
+  { transform: "translate(920 548) rotate(17) scale(0.86 0.46)" },
+  { transform: "translate(1198 534) rotate(-18) scale(0.58 0.86)", variant: "channel" },
+  { transform: "translate(1448 558) rotate(9) scale(0.52 0.42)" },
+  { transform: "translate(66 764) rotate(7) scale(0.46 0.68)" },
+  { transform: "translate(334 782) rotate(-13) scale(0.78 0.38)", variant: "channel" },
+  { transform: "translate(603 760) rotate(22) scale(0.48 0.74)" },
+  { transform: "translate(858 786) rotate(-10) scale(0.38 0.46)" },
+  { transform: "translate(1095 764) rotate(11) scale(0.74 0.34)" },
+  { transform: "translate(1398 782) rotate(-20) scale(0.62 0.78)" },
+  { transform: "translate(1602 742) rotate(12) scale(0.4 0.58)" },
 ];
 
-const SURVEY_MARKS = [
-  "translate(92 164)",
-  "translate(356 170)",
-  "translate(672 150)",
-  "translate(930 162)",
-  "translate(1268 176)",
-  "translate(1510 158)",
-  "translate(172 414)",
-  "translate(420 430)",
-  "translate(760 410)",
-  "translate(1040 424)",
-  "translate(1330 430)",
-  "translate(1546 408)",
-  "translate(244 694)",
-  "translate(540 704)",
-  "translate(834 690)",
-  "translate(1168 708)",
-  "translate(1476 690)",
-];
+const GRID_COLUMNS = [92, 330, 568, 806, 1044, 1282, 1520];
+const GRID_ROWS = [126, 324, 522, 720];
 
 function ContourIsland({
   transform,
-  size,
+  variant,
 }: {
   transform: string;
-  size?: string;
+  variant?: string;
 }) {
-  const loops = size === "small" ? SMALL_CONTOUR_LOOPS : CONTOUR_LOOPS;
+  const loops = variant === "channel" ? CHANNEL_LOOPS : CONTOUR_LOOPS;
 
   return (
-    <g className={`topographic-map__island${size === "small" ? " topographic-map__island--small" : ""}`} transform={transform}>
+    <g className={`topographic-map__island${variant === "channel" ? " topographic-map__island--channel" : ""}`} transform={transform}>
       {loops.map((path, index) => (
         <path d={path} key={index} />
       ))}
@@ -91,23 +73,34 @@ export function ShaderGradientBackground() {
         preserveAspectRatio="xMidYMid slice"
         viewBox="0 0 1600 900"
       >
+        <g className="topographic-map__grid">
+          {GRID_COLUMNS.map((x) => (
+            <line key={`col-${x}`} x1={x} x2={x} y1="-60" y2="960" />
+          ))}
+          {GRID_ROWS.map((y) => (
+            <line key={`row-${y}`} x1="-80" x2="1680" y1={y} y2={y} />
+          ))}
+        </g>
+
         <g className="topographic-map__terrain">
-          <path d="M-80 210C54 168 140 172 230 226C330 287 430 274 514 189C597 106 710 95 814 157C907 213 1010 207 1108 138C1214 64 1338 72 1436 156C1518 226 1598 234 1680 192" />
-          <path d="M-74 372C74 312 180 326 274 411C365 493 491 482 590 386C688 291 818 282 916 363C1013 444 1135 436 1234 350C1329 268 1448 267 1546 346C1608 396 1658 405 1712 386" />
-          <path d="M-92 640C40 585 154 596 258 676C368 761 494 762 611 676C726 591 844 590 954 672C1067 755 1196 760 1302 682C1414 600 1541 609 1678 712" />
+          <path d="M-80 205C62 169 151 190 246 244C355 306 434 258 514 183C602 101 716 102 814 156C905 206 1000 199 1110 136C1218 74 1338 86 1432 155C1514 216 1598 222 1680 190" />
+          <path d="M-74 372C72 318 176 338 276 414C366 482 487 492 590 389C692 287 815 288 918 365C1014 437 1135 447 1236 352C1328 266 1446 276 1544 347C1610 395 1660 400 1712 382" />
+          <path d="M-92 640C42 596 152 608 260 676C371 746 488 773 612 676C726 588 844 603 954 673C1070 747 1190 768 1302 684C1414 600 1540 622 1678 711" />
         </g>
 
         {CONTOUR_ISLANDS.map((island) => (
           <ContourIsland key={island.transform} {...island} />
         ))}
 
-        <g className="topographic-map__survey">
-          {SURVEY_MARKS.map((transform) => (
-            <g key={transform} transform={transform}>
-              <path d="M-14 0H14" />
-              <path d="M0-14V14" />
-            </g>
-          ))}
+        <g className="topographic-map__crosses">
+          {GRID_ROWS.flatMap((y) =>
+            GRID_COLUMNS.map((x) => (
+              <g key={`${x}-${y}`} transform={`translate(${x} ${y})`}>
+                <path d="M-12 0H12" />
+                <path d="M0-12V12" />
+              </g>
+            )),
+          )}
         </g>
       </svg>
     </div>
